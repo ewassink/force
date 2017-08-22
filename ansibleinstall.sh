e20c9988b94804d9165b82699504f3a080210a9b
Edwin Wassink <ewassink@hotmail.com>
2017-08-22 13:45:27 +0200

    initial

---
+++ ansibleinstall.sh
@@ -0,0 +1,6 @@
+sudo apt-get update && sudo apt-get install -y libssl-dev libffi-dev python-dev python-pip
+pip install "azure==2.0.0rc5" msrestazure
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update && sudo apt-get install -y ansible
+sudo pip install pywinrm
