1c89d7bb53f9c1affb489d420133fa0df4fcbc42
ewassink <ewassink@hotmail.com>
2020-10-08 13:13:16 +0200

    Create PrepareWindows.ps1

---
+++ PrepareWindows.ps1
@@ -0,0 +1,17 @@
+#
+# This script is run at Windows installation time
+# It calls other scripts that prepares Windows and makes it ready for Ansible
+#
+# When run by hand on a new windows machine, use the following commands:
+#   wget -Outfile PrepareWindows.ps1 https://git.rabobank.nl/projects/FPXI/repos/azure-windows-ansible/raw/PrepareWindows.ps1
+#   Set-ExecutionPolicy Bypass -Scope Process -Force; .\PrepareWindows.ps1
+
+cd \windows\temp
+
+# Enable WinRM
+wget -Outfile ConfigureRemotingForAnsible.ps1 https://git.rabobank.nl/projects/FPXI/repos/azure-windows-ansible/raw/ConfigureRemotingForAnsible.ps1
+.\ConfigureRemotingForAnsible.ps1 2>&1 >> PrepareWindows.log
+
+# Install Chocolatey package manager
+wget -Outfile InstallChocolatey.ps1 https://git.rabobank.nl/projects/FPXI/repos/azure-windows-ansible/raw/InstallChocolatey.ps1
+.\InstallChocolatey.ps1 2>&1 >> PrepareWindows.log
