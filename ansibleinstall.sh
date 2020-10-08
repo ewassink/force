bc8de707e7e3aed9c0eb09b06a7d812908b8d4b1
ewassink <ewassink@hotmail.com>
2020-10-08 13:25:08 +0200

    Delete ansibleinstall.sh

--- ansibleinstall.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get update && sudo apt-get install -y libssl-dev libffi-dev python-dev python-pip
-pip install "azure==2.0.0rc5" msrestazure
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get update && sudo apt-get install -y ansible
-sudo pip install pywinrm
-sudo apt-get install python-dnspython
-sudo ansible-galaxy install geerlingguy.jenkins geerlingguy.java
